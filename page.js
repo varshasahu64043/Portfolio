896750e5c879c6bcbb67a729284605d1140b5d9b
Varsha Sahu <varshasahu64043@gmail.com>
2025-02-26 11:55:29 +0530

    Add files via upload

---
+++ page.js
@@ -0,0 +1,9 @@
+export default function Test() {
+    return (
+      <div className="min-h-screen bg-blue-500 text-white p-8">
+        <h1 className="text-4xl font-bold">Test Page</h1>
+        <p className="mt-4 hover:text-yellow-300">Hover over me!</p>
+      </div>
+    );
+  }
+  
